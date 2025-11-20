56e945ed1f59d5db9807f89a73fa1c33ab0fdc9a
Nikhitha Natha <143377805+nikhithanatha123@users.noreply.github.com>
2025-11-20 22:06:11 +0530

    Create EvenOddChecker.java

---
+++ EvenOddChecker.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EvenOddChecker {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Even or Odd Checker ===");
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even.");
+        } else {
+            System.out.println(number + " is Odd.");
+        }
+
+        sc.close();
+    }
+}
